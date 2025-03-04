c3ab2b2650c4751a002c9082aec2121e93c6deeb
tijnjh <tijnjh@proton.me>
2025-03-05 00:43:05 +0100

    refactor: :lipstick: make the ui structure make more sense

--- src/routes/[slug]/+page.svelte
+++ src/routes/[slug]/+page.svelte
@@ -1,6 +1,10 @@
 <script lang="ts">
 	let { data }: { data: any } = $props();
+	import { browser } from '$app/environment';
 	import { page } from '$app/state';
+
+	import Search from 'lucide-svelte/icons/search';
+	import Link from 'lucide-svelte/icons/link';
 
 	const r = data.data;
 
@@ -18,44 +22,75 @@
 </svelte:head>
 
 <main class="grid max-w-3xl grid-cols-1 gap-6 p-6 lg:[margin-left:min(calc(20vw/2),10rem)]">
-	<aside>
-		<h1 class="text-nord6/50 mb-6">{r.Heading}</h1>
-		<div class="flex flex-col gap-6 md:flex-row">
-			{#if r.Image}
-				<img
-					src={`https://duckduckgo.com${r.Image}`}
-					alt={r.Heading}
-					class="mb-2 h-fit w-32 rounded object-cover object-top md:order-1"
-				/>
-			{/if}
+	<a href="/" class="text-nord6/50 mb-6 flex items-center gap-2">
+		<Search class="size-4" />
+		<span>{decodeURIComponent(page.url.pathname).replaceAll('/', '')}</span>
+	</a>
 
-			<p>
-				{r.Abstract}
-			</p>
-		</div>
+	{#if r.Heading}
+		<h1>{r.Heading}</h1>
+	{/if}
 
-		<a href={r.AbstractURL} class="text-nord6/50 block">
-			-> from {r.AbstractSource}
-		</a>
-	</aside>
+	{#if r.Abstract}
+		<aside>
+			<div class="flex flex-col gap-6 md:flex-row">
+				{#if r.Image}
+					<img
+						src={`https://duckduckgo.com${r.Image}`}
+						alt={r.Heading}
+						class="mb-2 h-fit w-32 rounded object-cover object-top md:order-1"
+					/>
+				{/if}
 
-	{#if r.RelatedTopics}
-		<h2 class="text-nord6/50">related</h2>
+				<p>
+					{r.Abstract}
+				</p>
+			</div>
+		</aside>
+	{/if}
+
+	{#if r.Results.length}
+		<h2 class="text-nord6/50">links</h2>
 
 		<ul>
-			{#each r.RelatedTopics as topic}
-				{#if topic.FirstURL}
-					{@const query = topic.FirstURL.split('/').reverse()[0]}
+			{#each r.Results as result}
+				{#if result.FirstURL}
 					<li class="mb-6">
-						<a href={`/${query.replaceAll('_', '+')}`}>
-							<h3>-> {query}</h3>
+						<a href={result.FirstURL} class="group flex items-center gap-2">
+							<Link class="size-4 shrink-0" />
+
+							<span class="truncate group-hover:underline">
+								{result.Text}
+							</span>
 						</a>
-						<p class="text-nord6/50">{topic.Text}</p>
 					</li>
 				{/if}
 			{/each}
 		</ul>
-	{:else}
-		<h2 class="text-nord6/50">no results found :(</h2>
+	{/if}
+
+	{#if r.RelatedTopics.length}
+		<h2 class="text-nord6/50">related queries</h2>
+
+		<ul>
+			{#each r.RelatedTopics as topic}
+				{#if topic.FirstURL}
+					{@const query = topic.FirstURL.split('/')
+						.reverse()[0]
+						.replaceAll('_', '%20')
+						.toLowerCase()}
+					<li class="mb-6">
+						<a href={`/${query}`} class="group flex items-center gap-2">
+							<Search class="size-4 shrink-0" />
+							<div class="truncate">
+								<span class="truncate group-hover:underline">
+									{topic.Text.split(' - ')[0]}
+								</span>
+							</div>
+						</a>
+					</li>
+				{/if}
+			{/each}
+		</ul>
 	{/if}
 </main>
