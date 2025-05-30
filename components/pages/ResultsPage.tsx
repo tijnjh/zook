9c07377fac73431220cd33d3ed7f53d9ec5ce50d
tijn <tijn@tijn.dev>
2025-05-31 01:01:27 +0200

    feat: add image zooming to results page

--- components/pages/ResultsPage.tsx
+++ components/pages/ResultsPage.tsx
@@ -1,6 +1,8 @@
 import { DdgResponse } from "@/lib/types";
 import { LinkIcon, SearchIcon } from "lucide-react";
 import Link from "next/link";
+import Zoom from "react-medium-image-zoom";
+import "react-medium-image-zoom/dist/styles.css";
 
 function stripTrailingSlash(str: string) {
   return str.endsWith("/") ? str.slice(0, -1) : str;
@@ -18,18 +20,20 @@ export default function ResultsPage({
       {!!result.Heading && <h1>{result.Heading}</h1>}
 
       {!!result.Abstract && (
-        <aside>
-          <div className="flex md:flex-row flex-col gap-6">
-            {!!result.Image && (
-              <img
-                src={`https://duckduckgo.com${result.Image}`}
-                alt={result.Heading}
-                className="md:order-1 mb-2 rounded w-32 h-fit object-cover object-top"
-              />
-            )}
+        <aside className="flex md:flex-row flex-col gap-6">
+          {!!result.Image && (
+            <div className="md:order-1 mb-2 rounded shrink-0">
+              <Zoom>
+                <img
+                  src={`https://duckduckgo.com${result.Image}`}
+                  alt={result.Heading}
+                  className="w-32 h-fit object-cover object-top"
+                />
+              </Zoom>
+            </div>
+          )}
 
-            <p>{result.Abstract}</p>
-          </div>
+          <p>{result.Abstract}</p>
         </aside>
       )}
 
@@ -62,45 +66,45 @@ export default function ResultsPage({
         </>
       )}
 
-      {result.RelatedTopics?.length
-        ? (
-          <>
-            <h2 className="text-nord6/50">related queries</h2>
+      {result.RelatedTopics?.length ? (
+        <>
+          <h2 className="text-nord6/50">related queries</h2>
 
-            <ul>
-              {result.RelatedTopics.map((topic) => {
-                if (!topic.FirstURL) {
-                  return null;
-                }
-
-                const q = topic.FirstURL.split("/")
-                  .reverse()[0]
-                  .replaceAll("_", "%20")
-                  .toLowerCase();
-
-                if (decodeURIComponent(q) !== query) {
-                  return (
-                    <li key={q} className="mb-6">
-                      <Link
-                        href={`/?q=${q}`}
-                        className="group flex items-center gap-2"
-                      >
-                        <SearchIcon className="size-4 shrink-0" />
-                        <div className="truncate">
-                          <span className="group-hover:underline truncate">
-                            {decodeURIComponent(q)}
-                          </span>
-                        </div>
-                      </Link>
-                    </li>
-                  );
-                }
+          <ul>
+            {result.RelatedTopics.map((topic) => {
+              if (!topic.FirstURL) {
                 return null;
-              })}
-            </ul>
-          </>
-        )
-        : <h3>No results found :(</h3>}
+              }
+
+              const q = topic.FirstURL.split("/")
+                .reverse()[0]
+                .replaceAll("_", "%20")
+                .toLowerCase();
+
+              if (decodeURIComponent(q) !== query) {
+                return (
+                  <li key={q} className="mb-6">
+                    <Link
+                      href={`/?q=${q}`}
+                      className="group flex items-center gap-2"
+                    >
+                      <SearchIcon className="size-4 shrink-0" />
+                      <div className="truncate">
+                        <span className="group-hover:underline truncate">
+                          {decodeURIComponent(q)}
+                        </span>
+                      </div>
+                    </Link>
+                  </li>
+                );
+              }
+              return null;
+            })}
+          </ul>
+        </>
+      ) : (
+        <h3>No results found :(</h3>
+      )}
     </>
   );
 }
